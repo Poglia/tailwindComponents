6cd9e9594c28ada5d0c55826d272863501ed05b6
Poglia <pedro.poglia@hotmail.com>
2025-10-16 13:05:11 -0300

    feat: Create Loading

---
+++ app/components/CircleLoad.tsx
@@ -0,0 +1,5 @@
+export const CircleLoad = () => {
+  return (
+    <div className="w-12 h-12 rounded-full border-8 border-blue-200 border-t-blue-600 border-r-blue-600 animate-spin"></div>
+  );
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
+import { CircleLoad } from "./components/CircleLoad";
 import { LoginScreen } from "./components/LoginScreen";
 
 export default function Home() {
   return (
-    <LoginScreen />
+    <CircleLoad />
   );
 }
